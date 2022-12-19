d6326706eca991d288a108f653d550891916bf5a
YY-NU <116430235+YYtech15@users.noreply.github.com>
2022-12-20 01:50:47 +0900

    各種ファイルの実装

---
+++ config.py
@@ -0,0 +1,11 @@
+'''
+api使用に必要な下記要素をそれぞれ記述してください
+    ・アプリID/デベロッパーID
+    ・application_secret
+    ・アフィリエイトID
+'''
+CLIENT_ME = {
+    'APPLICATION_ID':'1072507588405161320',
+    'APPLICATION_SECRET':'9133290c1a48f82ceb234bc0ade61fb516dbfc5b',
+    'AFF_ID':'2e2b208a.aa463515.2e2b208b.9d898007'
+}
---
+++ get_item_info.py
@@ -0,0 +1,102 @@
+from ast import keyword
+# 取得したデータ操作とcsv出力のため : pandas
+import pandas as pd
+import numpy as np
+# APIを叩く用 ： request
+import requests,json,datetime,os,re
+from time import sleep
+from config import CLIENT_ME
+
+# リクエストするURL()
+REQUEST_URL = 'https://app.rakuten.co.jp/'\
+'services/api/IchibaItem/Search/20170706'
+WANT_ITEMS = [
+    'itemName',         # 商品名
+    'itemPrice',        # 価格
+    'itemUrl'           # 商品url
+    # 'postageFlag'  送料フラグ 0->全て,1->送料込み
+]
+
+MAX_PAGE = 5
+HITS_PER_PAGE = 30
+
+# 日時指定
+start_time = datetime.datetime.today()
+this_date = format(start_time,'%Y%m%d')
+path_output_dir = f'./output/{this_date}'
+
+# リクエストを送る際のパラメータをdict型で書く
+req_params = {
+    'applicationId':CLIENT_ME['APPLICATION_ID'],    # 楽天の開発者向けページで取得したアプリID
+    'format':'json',        # 
+    'formatVersion':'2',    # 
+    'keyword':'',           # 検索したい文字列を指定
+    'hits':HITS_PER_PAGE,   # 
+    'sort':'+itemPrice',    # 
+    'page':0,               # 取得するページ　->　ループを回すことで複数ページの商品情報取得可
+    'minPrice':100          # 
+}
+
+def main():
+
+    #実行日日付フォルダ作成
+    if not os.path.isdir(path_output_dir):
+        os.mkdir(path_output_dir)
+
+    #商品記載テキストファイルからキーワード配列作成
+    with open('.\list_item_name.txt','r',encoding='utf-8') as f:
+        # 改行ごとに読み取る
+        item_info = list(map(str,f.read().split('\n')))
+
+    create_output_data(item_info)
+
+    print(f"{'-'*10}")
+
+# 出力データを作る関数
+def create_output_data(arg_item_info):
+
+    #キーワードループ
+    for keyword in arg_item_info:
+
+        #初期設定
+        count = 1
+        keyword = keyword.replace('\u3000',' ')
+        req_params['keyword'] = keyword
+        path_file = f'{path_output_dir}/{keyword}.csv'
+        df = pd.DataFrame(columns=WANT_ITEMS)
+
+
+        print(f"{'-'*10}\nNowSearchWord --> {keyword}")
+
+        #ページループ
+        while True:
+
+            req_params['page'] = count
+            response = requests.get(REQUEST_URL,req_params)
+            response_code = response.status_code
+            result = response.json()
+
+            if response_code != 200:
+                print(f"ErrorCode --> {response_code}\nError --> {result['error']}\nPage --> {count}")
+            else:
+
+                #返ってきた商品数の数が0の場合はループ終了
+                if result['hits'] == 0:
+                    break
+
+                tmp_df = pd.DataFrame(result['Items'])[WANT_ITEMS]
+                df = pd.concat([df,tmp_df],ignore_index=True)
+
+            if count == MAX_PAGE:
+                break
+
+            count += 1
+
+            #リクエスト制限回避
+            sleep(1)
+
+        df.to_csv(path_file,index=False,encoding="utf_8_sig",sep=",")
+        print(f"Finished!!")
+
+if __name__ == '__main__':
+    main()
