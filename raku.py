cb0980583bef8811bcd8d3f607c779aad75ebec3
YYtech15 <midori1140flower@gmail.com>
2022-12-23 19:17:44 +0900

    get関数の修正かつ完成

--- raku.py
+++ raku.py
@@ -11,73 +11,52 @@ class Rakuten:
 
         # 取得するデータ項目
         self.Want_Items = ["itemName", "itemPrice", "itemUrl", "url",  "shopName", "itemCaption", "postageFlag"]
-        self.MAX_PAGE = 5
+        self.MAX_PAGE = 1
 
         # getでリクエストを送る際のパラメータ
-        self.req_params_get = {
+        self.req_params = {
             "applicationId": "1072507588405161320",
             "format":"json",
             "formatVersion":"2",
             "keyword":"",
             "hits": 30,
-            "sort":"+itemPrice",
-            "page": 0
-        }
-        # searchでリクエストを送る際のパラメータ
-        self.req_params_search = {
-            "applicationId": "1072507588405161320",
-            "format": "json",
-            "formatVersion": "2",
-            "keyword": "",
-            "hits": 30,
             "sort": "-reviewAverage",
-            "page": 0
+            "page": 0,
+            "NGKeyword" : "中古"
         }
 
     # 検索データ取得
-    def get(self, Jan_code):
+    def get(self, *Jan_code:str):
         result = dict()
         for j in Jan_code:
             # 正しいJANコードか判定
             if len(j) != 8 and len(j) != 13:
                 result[j] = {"status": False}
                 continue
-            cnt = 1
-            self.req_params_get['keyword'] = j
+            self.req_params['keyword'] = j
+            self.req_params['page'] = 1
 
-            #ページループ
-            while True:
-                self.req_params_get['page'] = cnt
-                # APIを実行してreq_paramsのデータを取得
-                res = requests.get(self.REQUEST_URL, self.req_params_get)
-                response_code = res.status_code             # ステータスコード取得
-                data = res.json()                           # jsonにデコードする
-
-                if response_code != 200:
-                    # エラー出力
-                    print(f"ErrorCode --> {response_code}\nError --> {data['error']}")
-                    break
-                else:
-                    #返ってきた商品数の数が0の場合はループ終了
-                    if res['hits'] == 0:
-                        break
-
-                    if data["itemPrice"] < minPrice:
-                        minPrice = data["itemPrice"]
-
-                    result[j] = {
-                        'name' : data['itemName'],
-                        'price' : data['itemPrice'],
-                        'url' : data['itemUrl'],
-                        'seller' : data['shopName'],
-                        'shipping' : data['postageFlag']
-                    }
-
-                if cnt == self.MAX_PAGE:
-                    break
-                cnt += 1
-                #リクエスト制限回避
-                sleep(1)
+            # APIを実行してreq_paramsのデータを取得
+            res = requests.get(self.REQUEST_URL, self.req_params)
+            response_code = res.status_code             # ステータスコード取得
+            data = res.json()                           # jsonにデコードする
+            if response_code != 200:
+                # エラー出力
+                print(f"ErrorCode --> {response_code}\nError --> {data['error']}")
+                break
+            minPrice = data["Items"][0]['itemPrice']
+            index = 0
+            for k in range(data["hits"]):
+                if data["Items"][k]['itemPrice'] < minPrice:
+                    minPrice = data["Items"][k]['itemPrice']
+                    index = k
+            result[j] = {
+                'name' : data["Items"][index]['itemName'],
+                'price' : data["Items"][index]['itemPrice'],
+                'url' : data["Items"][index]['itemUrl'],
+                'seller' : data["Items"][index]['shopName'],
+                'shipping' : data["Items"][index]['postageFlag']
+            }
         return result
 
 
@@ -85,14 +64,14 @@ class Rakuten:
     # keywordは最大128文字の１バイト文字
     def search(self, keyword: str):
         # 検索ワード
-        self.req_params_search['keyword'] = keyword
+        self.req_params['keyword'] = keyword
         cnt = 1
 
         #ページループ
         while True:
-            self.req_params_search['page'] = cnt
+            self.req_params['page'] = cnt
             # APIを実行してreq_paramsのデータを取得
-            res = requests.get(self.REQUEST_URL, self.req_params_search)
+            res = requests.get(self.REQUEST_URL, self.req_params)
             response_code = res.status_code             # ステータスコード取得
             data = res.json()                           # jsonにデコードする
             result = list()
@@ -111,8 +90,9 @@ class Rakuten:
                 item = {
                     'name' : data['itemName'],
                     'price' : data['itemPrice'],
-                    'janCode': self.extract_JanCode(data['itemUrl']),
+                    'janCode': self.extract_JanCode(data['itemCaption']),
                     'url' : data['itemUrl'],
+                    'image' : data['mediumImageUrls'],
                     'seller' : data['shopName'],
                     'shipping' : data['postageFlag']
                 }
@@ -131,12 +111,12 @@ class Rakuten:
         pattern = list()
         if idx != -1:
             pattern = re.split('[:(/\■)【：】]', itemCaption[idx+1:idx+30])
-            for i in pattern:
-                # JANコードがあれば返り値で返す
-                if :
-                    return i
-                else:
-                    return ""
+            # for i in pattern:
+            #     # JANコードがあれば返り値で返す
+            #     if :
+            #         return i
+            #     else:
+            #         return ""
         else:
             return ""
 
@@ -153,9 +133,9 @@ if __name__ == '__main__':
     with open("config_rakuten.json", "r") as f:
         config = json.load(f)
     rakuten = Rakuten(config["Rakuten_App_ID"])
-    response = rakuten.get(janCodes)
+    res = rakuten.get(*janCodes)
 
     with open("sample.json", "w") as f:
-        json.dump(response, f, ensure_ascii=False, indent=4)
+        json.dump(res, f, ensure_ascii=False, indent=4)
 
-    print(response)
+    print(res)
